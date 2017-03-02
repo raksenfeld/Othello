46e3099291180b981f49944937727fa10baaa94d
Rita <rita.aksenfeld@gmail.com>
2017-03-01 21:57:07 -0800

    Modified player.cpp

--- player.cpp
+++ player.cpp
@@ -1,6 +1,6 @@
 #include "player.hpp"
 
-/*
+/**
  * Constructor for the player; initialize everything here. The side your AI is
  * on (BLACK or WHITE) is passed in as "side". The constructor must finish
  * within 30 seconds.
@@ -16,13 +16,13 @@ Player::Player(Side side) {
      */
 }
 
-/*
+/**
  * Destructor for the player.
  */
 Player::~Player() {
 }
 
-/*
+/**
  * Compute the next move given the opponent's last move. Your AI is
  * expected to keep track of the board on its own. If this is the first move,
  * or if the opponent passed on the last move, then opponentsMove will be
